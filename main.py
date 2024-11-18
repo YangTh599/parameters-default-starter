b8b4949c2dc21c1ae19d41d2e64383951648cab5
YangTh599 <YangTh599@northwested.org>
2024-11-18 10:32:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-# Student name
-# Current date
+# Thayer Yang  
+# 18 NOV 2024
 # Default Values for Parameters Practice
 
 # 8-3 (T-Shirt)
 
+# def make_shirt(size, message):
+#     print(f'Shirt Order: a {size} shirt with the message: "{message}"')
 
+# make_shirt("large", "I LOVE VIDEO GAMES")
+# make_shirt(message="I love numbers!", size="XXL")
 
 
 # 8-4 (Large Shirts)
 
+def make_shirt(size = "large", message= "I love Python"):
+    print(f'Shirt Order: a {size} shirt with the message: "{message}" ')
 
-
+make_shirt()
+make_shirt(size="medium")
+make_shirt(size="small",message="MINCERAFT")
 
 # 8-5 (Cities)
+
+def describe_city(country = "Canada", city= "Ottawa"):
+    print(f'{city.title()} is in {country.title()}')
+
+describe_city(city="Toronto")
+describe_city(city="Detroit")
+describe_city(city="Paris")
